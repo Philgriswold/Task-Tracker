56ba18c1da34275226d6c15dea3a33f25084d960
Phil Griswold <philgriswold@me.com>
2023-03-09 12:56:24 -0600

    got backend working

--- src/App.vue
+++ src/App.vue
@@ -45,12 +45,12 @@ export default {
     }
   },
   async fetchTasks() {
-    const res = await fetch("https://localhost:5000/tasks");
+    const res = await fetch("http://localhost:5000/tasks");
     const data = await res.json();
     return data;
   },
   async fetchTask(id) {
-    const res = await fetch(`https://localhost:5000/tasks`);
+    const res = await fetch(`http://localhost:5000/tasks`);
     const data = await res.json();
     return data;
   },
